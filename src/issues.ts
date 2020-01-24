19db31cd693437561d3d0e4b8934136d1223d0c4
Franck Chauvel <franck.chauvel@sintef.no>
2020-01-24 10:33:33 +0100

    Configure: Add License header to each source file

--- src/issues.ts
+++ src/issues.ts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (C) 2019, 2020 Franck Chauvel
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.
+ *
+ * See the LICENSE file for details.
+ */
+
 export class ErrorCode {
     public static readonly TYPE_ERROR = "TYPE ERROR";
     public static readonly MISSING_PROPERTY = "MISSING PROPERTY";
